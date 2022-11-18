e932c2a018c81686ac47307507d8442b93ddef8c
Pawan <pawanbajaj2012@gmail.com>
2022-11-18 13:19:02 +0530

    frontend issues solved

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,13 @@
 import { BigNumber, Contract, providers, utils } from "ethers";
-import Head from "next/Head";
-import React, {useEffect, useRef, useState} from "react";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import {
-  NFT_CONTRACT_ADDRESS,
   NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
   TOKEN_CONTRACT_ADDRESS,
-  TOKEN_CONTRACT_ABI
 } from "../constants";
-
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -16,7 +15,8 @@ export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
   const [loading, setLoading] = useState(false);
   const [tokensToBeClaimed, setTokensToBeClaimed] = useState(zero);
-  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] = useState(zero);
+  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] =
+    useState(zero);
   const [tokenAmount, setTokenAmount] = useState(zero);
   const [tokensMinted, setTokensMinted] = useState(zero);
   const [isOwner, setIsOwner] = useState(false);
@@ -25,142 +25,164 @@ export default function Home() {
   const getTokensToBeClaimed = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, provider);
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, provider);
-
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
       const signer = await getProviderOrSigner(true);
       const address = await signer.getAddress();
       const balance = await nftContract.balanceOf(address);
       if (balance === zero) {
         setTokensToBeClaimed(zero);
-      }
-      else {
+      } else {
         var amount = 0;
-        for(var i = 0; i < balance;i++) {
+        for (var i = 0; i < balance; i++) {
           const tokenId = await nftContract.tokenOfOwnerByIndex(address, i);
           const claimed = await tokenContract.tokenIdsClaimed(tokenId);
-          if(!claimed) {
+          if (!claimed) {
             amount++;
           }
         }
+
         setTokensToBeClaimed(BigNumber.from(amount));
       }
-      
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
+      setTokensToBeClaimed(zero);
+    }
+  };
 
   const getBalanceOfCryptoDevTokens = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, provider);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
       const signer = await getProviderOrSigner(true);
       const address = await signer.getAddress();
-
       const balance = await tokenContract.balanceOf(address);
       setBalanceOfCryptoDevTokens(balance);
-      
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
+      setBalanceOfCryptoDevTokens(zero);
+    }
+  };
 
   const mintCryptoDevToken = async (amount) => {
     try {
       const signer = await getProviderOrSigner(true);
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, signer);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
       const value = 0.001 * amount;
       const tx = await tokenContract.mint(amount, {
         value: utils.parseEther(value.toString()),
       });
       setLoading(true);
+
       await tx.wait();
       setLoading(false);
-      window.alert("Successfully minted Crypto Dev Tokens");
+      window.alert("Sucessfully minted Crypto Dev Tokens");
       await getBalanceOfCryptoDevTokens();
       await getTotalTokensMinted();
       await getTokensToBeClaimed();
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
+    }
+  };
 
   const claimCryptoDevTokens = async () => {
     try {
       const signer = await getProviderOrSigner(true);
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, signer);
-
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
       const tx = await tokenContract.claim();
       setLoading(true);
       await tx.wait();
       setLoading(false);
-
-      window.alert("Successfully Minted the Crypto Dev Tokens");
+      window.alert("Sucessfully claimed Crypto Dev Tokens");
       await getBalanceOfCryptoDevTokens();
       await getTotalTokensMinted();
       await getTokensToBeClaimed();
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
+    }
+  };
 
   const getTotalTokensMinted = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, provider);
-
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
       const _tokensMinted = await tokenContract.totalSupply();
       setTokensMinted(_tokensMinted);
-
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
+    }
+  };
 
   const getOwner = async () => {
     try {
       const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, provider);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
       const _owner = await tokenContract.owner();
       const signer = await getProviderOrSigner(true);
       const address = await signer.getAddress();
-      if(address.toLowerCase() === _owner.toLowerCase()) {
+      if (address.toLowerCase() === _owner.toLowerCase()) {
         setIsOwner(true);
       }
+    } catch (err) {
+      console.error(err.message);
     }
-    catch (err) {
-      console.error(err);
-    };
-  }
+  };
 
   const withdrawCoins = async () => {
     try {
       const signer = await getProviderOrSigner(true);
-      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, signer);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+
       const tx = await tokenContract.withdraw();
       setLoading(true);
       await tx.wait();
       setLoading(false);
       await getOwner();
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
+    }
   }
 
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
+
     const { chainId } = await web3Provider.getNetwork();
-    if(chainId !== 5) {
-      window.alert("Change the network to goerli");
-      throw new Error("Change the network to goerli");
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
     }
 
     if (needSigner) {
@@ -174,14 +196,12 @@ export default function Home() {
     try {
       await getProviderOrSigner();
       setWalletConnected(true);
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
-    };
-  }
-
+    }
+  };
   useEffect(() => {
-    if(!walletConnected) {
+    if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
         network: "goerli",
         providerOptions: {},
@@ -190,38 +210,58 @@ export default function Home() {
       connectWallet();
       getTotalTokensMinted();
       getBalanceOfCryptoDevTokens();
+      getTokensToBeClaimed();
       withdrawCoins();
     }
   }, [walletConnected]);
 
   const renderButton = () => {
-    if(loading) {
+    if (loading) {
       return (
         <div>
           <button className={styles.button}>Loading...</button>
         </div>
       );
     }
-
-    if(tokensToBeClaimed > 0) {
+    if (walletConnected && isOwner) {
       return (
         <div>
-          <div className={styles.description}>{tokensToBeClaimed * 10}Tokens can be claimed!</div>
-          <button className={styles.button} onClick={claimCryptoDevTokens}>Claim Tokens</button>
+          <button className={styles.button1} onClick={withdrawCoins}>
+            Withdraw Coins
+          </button>
         </div>
       );
     }
-
-    return (
-      <div style={{display: "flex-col"}}>
+    if (tokensToBeClaimed > 0) {
+      return (
         <div>
-          <input type="number" placeholder="Amount of tokens"
-          onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
-          className={styles.input}></input>
+          <div className={styles.description}>
+            {tokensToBeClaimed * 10} Tokens can be claimed!
+          </div>
+          <button className={styles.button} onClick={claimCryptoDevTokens}>
+            Claim Tokens
+          </button>
         </div>
-        <button className={styles.button}
-          disabled={!(tokenAmount>0)}
-          onClick={() => mintCryptoDevToken(tokenAmount)}>Mint Tokens</button>
+      );
+    }
+    return (
+      <div style={{ display: "flex-col" }}>
+        <div>
+          <input
+            type="number"
+            placeholder="Amount of Tokens"
+            onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+            className={styles.input}
+          />
+        </div>
+
+        <button
+          className={styles.button}
+          disabled={!(tokenAmount > 0)}
+          onClick={() => mintCryptoDevToken(tokenAmount)}
+        >
+          Mint Tokens
+        </button>
       </div>
     );
   };
@@ -230,8 +270,8 @@ export default function Home() {
     <div>
       <Head>
         <title>Crypto Devs</title>
-        <meta name="description" content="ICO-DAPP"/>
-        <link ref="icon" href="/favicon.ico"/>
+        <meta name="description" content="ICO-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
         <div>
@@ -241,22 +281,29 @@ export default function Home() {
           </div>
           {walletConnected ? (
             <div>
-              <div className={styles.description}>You have minted {utils.formatEther(balanceOfCryptoDevTokens)} Crypto Dev Tokens</div>
-              <div className={styles.description}>Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!</div>
+              <div className={styles.description}>
+                You have minted {utils.formatEther(balanceOfCryptoDevTokens)} Crypto
+                Dev Tokens
+              </div>
+              <div className={styles.description}>
+                Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+              </div>
               {renderButton()}
-            </div>  
+            </div>
           ) : (
-            <button onClick={connectWallet} className={styles.button}>Connect your wallet</button>
+            <button onClick={connectWallet} className={styles.button}>
+              Connect your wallet
+            </button>
           )}
         </div>
         <div>
-          <img className={styles.image} src = "./0.svg"/>
+          <img className={styles.image} src="./0.svg" />
         </div>
       </div>
+
       <footer className={styles.footer}>
-        Made with &#10084; by pxvvxn
+        Made with &#10084; by Crypto Devs
       </footer>
     </div>
   );
-
 }
